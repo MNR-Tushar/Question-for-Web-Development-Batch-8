095134363092bac6d8c0bb7789976da26d038d9d
Md Naimur Rahman <rahman23105101275@diu.edu.bd>
2025-05-05 16:48:22 +0600

    Modify problem 1

--- 1_student_grade_calculator.py
+++ 1_student_grade_calculator.py
@@ -1,28 +1,46 @@
+total_point=0
 
-def average(marks,total_sub):
-    sum=0
-    for i in marks:
-        sum=sum+i
-    try:
-        return sum/total_sub
-    except Exception as e:
-        print("Please, Write down total subject greater than \"0\"")
-
-def Grade(m):
-    if m>=80 and m<=100:
+def L_grade(point):
+    if point>=5.00:
         return "A+"
-    elif m>=70 and m<=79:
+    elif point>=4.00:
         return "A"
-    elif m>=60 and m<=69:
+    elif point>=3.5:
         return "A-"
-    elif m>=50 and m<=59:
+    elif point>=3.00:
         return 'B'
-    elif m>=40 and m<=49:
+    elif point>=2.00:
         return "C"
-    elif m>=33 and m<=39:
+    elif point>=1.00:
         return "D"
     else:
         return "F"
+    
+
+def Grade(m):
+    global total_point
+    if m>=80:
+        total_point+=5.00
+        return "A+"
+    elif m>=70:
+        total_point+=4
+        return "A"
+    elif m>=60:
+        total_point+=3.5
+        return "A-"
+    elif m>=50:
+        total_point+=3
+        return 'B'
+    elif m>=40:
+        total_point+=2
+        return "C"
+    elif m>=33:
+        total_point+=1
+        return "D"
+    else:
+        total_point+=0
+        return "F"
+
 print("Student Grade Calculator")
 while True:
     marks=[]
@@ -39,11 +57,21 @@ while True:
             marks[i]=int(input(f"Enter your {subject_name[i]} mark: "))
 
     
-    print("\nAverage Marks: ",average(marks,total_sub))
-    
+    #Display student information
+    print(f"\nStudent Name: {name}")
+    print("Grade All Subject")
+    print("------------------------")
     for sub in range(0,total_sub):
         print(f"Grade of {subject_name[sub]} : {Grade(marks[sub])}")
     
+    try:
+        G=total_point/total_sub
+        print("------------------------")
+        print(f"GPA: {G} Grade: {L_grade(G)}")
+    except:
+        print("Please input your total subject!")
+    
+    total_point=0
     print("\n")
 
 
